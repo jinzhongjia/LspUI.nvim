b153804e8e0cc241cff1c12faeb3ab61a3b20adc
jinzhongjia <mail@nvimer.org>
2023-08-05 00:32:32 +0800

    add func:`version`

--- lua/LSPUI/lib/util.lua
+++ lua/LSPUI/lib/util.lua
@@ -1,6 +1,8 @@
 local api = vim.api
 local M = {}
 
+local version = "v2-undefined"
+
 local key_bind_opt = { noremap = true, silent = true }
 local move_keys = { "h", "ge", "e", "0", "$", "l", "w", "b", "<Bs>", "j", "k", "<Left>", "<Right>", "<Up>", "<Down>" }
 local other_keys = { "x", "y", "v", "o", "O", "q" }
@@ -19,7 +21,11 @@ end
 -- generate command description
 --- @param desc string
 M.command_desc = function(desc)
-	return "[LspUI]: " .. desc
+    return "[LspUI]: " .. desc
+end
+
+M.version =function ()
+	return version
 end
 
 return M
