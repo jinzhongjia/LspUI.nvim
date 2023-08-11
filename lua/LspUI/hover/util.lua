9c64a920641d507eb495830eef50d92879447fea
jinzhongjia <mail@nvimer.org>
2023-08-11 20:32:48 +0800

    add util function for hover

--- lua/LspUI/hover/util.lua
+++ lua/LspUI/hover/util.lua
@@ -1,3 +1,14 @@
+local lsp = vim.lsp
+local hover_feature = lsp.protocol.Methods.textDocument_hover
+
 local M = {}
+
+-- get all valid clients for hover
+--- @param buffer_id integer
+--- @return lsp.Client[]|nil clients array or nil
+M.get_clients = function(buffer_id)
+	local clients = lsp.get_clients({ bufnr = buffer_id, method = hover_feature })
+	return #clients == 0 and nil or clients
+end
 
 return M
