eead6c218165ac9aaa9fe75ccd4e1b4428c0162e
jinzhongjia <mail@nvimer.org>
2023-08-14 20:11:53 +0800

    definition: add some function

--- lua/LspUI/definition/init.lua
+++ lua/LspUI/definition/init.lua
@@ -1,6 +1,9 @@
+local api = vim.api
 local config = require("LspUI.config")
 local command = require("LspUI.command")
 local lib_notify = require("LspUI.lib.notify")
+local util = require("LspUI.definition.util")
+local lib_lsp = require("LspUI.lib.lsp")
 local M = {}
 -- whether this module is initialized
 local is_initialized = false
@@ -26,6 +29,20 @@ M.run = function()
 		lib_notify.Info("definition is not enabled!")
 		return
 	end
+
+	-- get current buffer
+	local current_buffer = api.nvim_get_current_buf()
+	-- get current window
+	local current_window = api.nvim_get_current_win()
+
+	local clients = util.get_clients(current_buffer)
+	if clients == nil then
+		return
+	end
+
+	local params = util.make_params(current_window)
+
+	util.get_definition_tuple(current_buffer, clients, params, function() end)
 end
 
 return M
--- lua/LspUI/definition/util.lua
+++ lua/LspUI/definition/util.lua
@@ -1,5 +1,6 @@
 local lsp = vim.lsp
 local definition_feature = lsp.protocol.Methods.textDocument_definition
+local lib_lsp = require("LspUI.lib.lsp")
 
 local M = {}
 
@@ -9,6 +10,30 @@ local M = {}
 M.get_clients = function(buffer_id)
 	local clients = lsp.get_clients({ bufnr = buffer_id, method = definition_feature })
 	return #clients == 0 and nil or clients
+end
+
+-- make request param
+-- TODO: implement `WorkDoneProgressParams` and `PartialResultParams`
+--
+--- @param window_id integer
+--- @return lsp.TextDocumentPositionParams
+--- @see https://microsoft.github.io/language-server-protocol/specifications/lsp/3.17/specification/#definitionParams
+M.make_params = function(window_id)
+	return lsp.util.make_position_params(window_id)
+end
+
+--- @param buffer_id integer
+---@param clients lsp.Client[]
+---@param params lsp.TextDocumentPositionParams
+---@param callback function
+M.get_definition_tuple = function(buffer_id, clients, params, callback)
+	lib_lsp.lsp_clients_request(buffer_id, clients, definition_feature, params, function(data)
+		for _, val in pairs(data) do
+			--- @type lsp.Location|lsp.Location[]|lsp.LocationLink[]|nil
+			local definition_response = val.result
+			local client = val.client
+		end
+	end)
 end
 
 return M
