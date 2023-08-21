387e304a652bf9ffd05e1fe6d940115f71a45965
jinzhongjia <mail@nvimer.org>
2023-08-21 22:30:13 +0800

    add code_action register support

--- lua/LspUI/code_action/register.lua
+++ lua/LspUI/code_action/register.lua
@@ -1,1 +1,40 @@
 --- TODO: add register code action
+
+local M = {}
+
+--- @type {[string]:fun(uri:lsp.URI,range:lsp.Range):{title:string,action:function}[]}
+local list = {}
+
+-- title and callback
+--- @param name string
+--- @param callback fun(uri:lsp.URI,range:lsp.Range):{title:string,action:function}[]
+M.register = function(name, callback)
+    list[name] = callback
+end
+
+--- @param name string
+M.unregister = function(name)
+    if list[name] then
+        list[name] = nil
+    end
+end
+
+--- @param uri lsp.URI
+--- @param range lsp.Range
+--- @return {title:string,action:function}[]
+M.handle = function(uri, range)
+    --- @type {title:string,action:function}[]
+    local result = {}
+    for name, callback in pairs(list) do
+        if callback then
+            local res = callback(uri, range)
+            for _, val in pairs(res) do
+                table.insert(result, val)
+            end
+        end
+    end
+
+    return result
+end
+
+return M
