f0c6fd44b6f6fa66194373dcfed3eed5131b48fd
jinzhongjia <mail@nvimer.org>
2023-08-22 21:28:31 +0800

    add version detect, at least 0.10

--- lua/LspUI/init.lua
+++ lua/LspUI/init.lua
@@ -1,17 +1,22 @@
-local api = require("LspUI.api")
-local command = require("LspUI.command")
-local config = require("LspUI.config")
-local modules = require("LspUI.modules")
+local lib_notify = require("LspUI.lib.notify")
 
 return {
     -- init `LspUI` plugin
     --- @param user_config LspUI_config? user's plugin config
     setup = function(user_config)
-        config.setup(user_config)
-        command.init()
-        for _, module in pairs(modules) do
-            module.init()
+        if vim.fn.has("nvim-0.10") == 1 then
+            local command = require("LspUI.command")
+            local config = require("LspUI.config")
+            local modules = require("LspUI.modules")
+
+            config.setup(user_config)
+            command.init()
+            for _, module in pairs(modules) do
+                module.init()
+            end
+        else
+            lib_notify.Warn("The version of neovim needs to be at least 0.10!!")
         end
     end,
-    api = api.api,
+    api = vim.fn.has("nvim-0.10") == 0 and {} or require("LspUI.api"),
 }
