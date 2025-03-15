8e322f98d30aa36be12c9cd53d2e1ff31e31f7cf
jinzhongjia <mail@nvimer.org>
2025-03-16 01:11:18 +0800

    little change

--- lua/LspUI/lib/util.lua
+++ lua/LspUI/lib/util.lua
@@ -37,7 +37,7 @@ end
 -- check buffer is listed ?
 --- @param buffer_id integer
 --- @return boolean
-M.buffer_is_listed = function(buffer_id)
+function M.buffer_is_listed(buffer_id)
     return fn.buflisted(buffer_id) == 1
 end
 
