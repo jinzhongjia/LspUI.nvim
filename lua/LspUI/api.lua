a6adf44edd002e333b1ba1ae83a71b4bc191be94
Ailelix <46298182+ailelix@users.noreply.github.com>
2025-05-19 13:06:02 +0100

    Fix `api.lua`
    Correct rename = modules.rename.run

--- lua/LspUI/api.lua
+++ lua/LspUI/api.lua
@@ -18,7 +18,7 @@ local M = {}
 
 M.api = {
     code_action = modules.code_action.run,
-    rename = modules.code_action.run,
+    rename = modules.rename.run,
     diagnostic = modules.diagnostic.run,
     hover = modules.hover.run,
     definition = modules.definition.run,
